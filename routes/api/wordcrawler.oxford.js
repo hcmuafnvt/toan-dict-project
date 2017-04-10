eea35f933a57b35977418e0ae6e93a5f52769add
Toan NGUYEN <toan.nguyen@s60.com.au>
2017-04-10 22:49:41 +0700

    build oxford crawling

--- routes/api/wordcrawler.oxford.js
+++ routes/api/wordcrawler.oxford.js
@@ -54,6 +54,7 @@ router.get('/getword', keystone.middleware.api, function (req, res) {
         }
 
         var translateToEn = {};
+        // grambs
         if($mainContents.find('> .gramb').length > 0) {
             translateToEn.grambs = [];
             $mainContents.find('> .gramb').each(function(idx, gramb) {
@@ -63,19 +64,21 @@ router.get('/getword', keystone.middleware.api, function (req, res) {
                 if(grambObj.typeOfWord === '') return;
                 grambObj.level1 = [];
                 $gramb.find('.semb > li').each(function(idx, level1) {
+                    //level 1
                     var level1Obj = {};
                     var $level1 = $(level1);
-
                     level1Obj.mean = $level1.find('.trg p .ind').text();
-
+                    
                     level1Obj.exampls = [];
                     $level1.find('> .trg > .exg .ex').each(function() {
                         var ex = '';
-                        if($(this).find('.grammatical_note').length > 0) {
-                            ex = '<span class="no-object">' + $(this).find('.grammatical_note').text() + '<span>' + $(this).find('em').text();
-                        }  else {
-                             ex = $(this).find('em').text();
+                        if($(this).find('.form-groups').length > 0) {
+                            ex += '<span class="level1-strong-mean">' + $(this).find('.form-groups strong').text() + '</span>';
                         }
+                        if($(this).find('.grammatical_note').length > 0) {
+                            ex += '<span class="no-object">' + $(this).find('.grammatical_note').text() + '</span>';
+                        }                            
+                        ex += $(this).find('em').text();
                         level1Obj.exampls.push(ex);
                     });
                     if(level1Obj.exampls.length < 5) {
@@ -83,23 +86,127 @@ router.get('/getword', keystone.middleware.api, function (req, res) {
                         if($otherExamples.length > 0) {
                             $otherExamples.slice(0, 5 - level1Obj.exampls.length).each(function() {                                
                                 var ex = '';
-                                if($(this).find('.grammatical_note').length > 0) {
-                                    ex = '<span class="no-object">' + $(this).find('.grammatical_note').text() + '<span>' + $(this).find('em').text();
-                                }  else {
-                                    ex = $(this).find('em').text();
+                                if($(this).find('.form-groups').length > 0) {
+                                    ex += '<span class="level1-strong-mean">' + $(this).find('.form-groups strong').text() + '</span>';
                                 }
+                                if($(this).find('.grammatical_note').length > 0) {
+                                    ex += '<span class="no-object">' + $(this).find('.grammatical_note').text() + '</span>';
+                                }                            
+                                ex += $(this).find('em').text();
                                 level1Obj.exampls.push(ex);
                             });
                         } 
                     }
 
+                    //level 2
+                    level1Obj.level2 = [];
+                    $level1.find('.subSense').each(function(idx, level2) {
+                        var level2Obj = {};
+                        var $level2 = $(level2);
+                        
+                        var level2Mean = '';
+                        if($level2.find('.form-groups').length > 0) {
+                            level2Mean += '<span class="level2-strong-mean">' + $level2.find('.form-groups strong').text() + '</span>';
+                        }
+                        if($level2.find('.grammatical_note').length > 0) {
+                            level2Mean += '<span class="no-object">' + $level2.find('.grammatical_note').text() + '</span>';
+                        }                            
+                        level2Mean += $level2.find('.ind').text();
+                        level2Obj.mean = level2Mean;
+                        
+                        level2Obj.exampls = [];
+                        $level2.find('> .trg > .exg .ex').each(function() {
+                            var ex = '';
+                            if($(this).find('.form-groups').length > 0) {
+                                ex += '<span class="level2-strong-ex">' + $(this).find('.form-groups strong').text() + '</span>';
+                            }
+                            if($(this).find('.grammatical_note').length > 0) {
+                                ex += '<span class="no-object">' + $(this).find('.grammatical_note').text() + '</span>';
+                            }                            
+                            ex += $(this).find('em').text();
+                            level2Obj.exampls.push(ex);
+                        });
+                        if(level2Obj.exampls.length < 2) {
+                            var $otherExamples = $level2.find('> .trg > .examples .ex');
+                            if($otherExamples.length > 0) {
+                                $otherExamples.slice(0, 2 - level2Obj.exampls.length).each(function() {                                
+                                    var ex = '';
+                                    if($(this).find('.form-groups').length > 0) {
+                                        ex += '<span class="level2-strong-mean">' + $(this).find('.form-groups strong').text() + '</span>';
+                                    }
+                                    if($(this).find('.grammatical_note').length > 0) {
+                                        ex += '<span class="no-object">' + $(this).find('.grammatical_note').text() + '</span>';
+                                    }                            
+                                    ex += $(this).find('em').text();
+                                    level2Obj.exampls.push(ex);
+                                });
+                            } 
+                        }
+                        level1Obj.level2.push(level2Obj);
+                    });
+
                     grambObj.level1.push(level1Obj);
                 })
                 translateToEn.grambs.push(grambObj);
-            });
-
-            console.log(JSON.stringify(translateToEn));
+            });            
         }
+
+        // etymology
+        if($mainContents.find('> .etymology').length > 0) {
+            translateToEn.etymologys = [];
+            $mainContents.find('> .etymology').each(function(idx, etymology) {
+                var etymologyObj = {};
+                var $etymology = $(etymology);                
+                etymologyObj.typeOfWord = $etymology.find('.phrases-title').first().text();
+                if(etymologyObj.typeOfWord === '') return;
+                etymologyObj.level1 = [];
+                var $listPhases = $etymology.find('.gramb > li');
+                var $listMeansAndEx = $etymology.find('.gramb .semb');
+                for(var i = 0; i < $listPhases.length; i++) {                
+                    //level 1
+                    var level1Obj = {};
+                    var $level1Phase = $listPhases.eq(i);
+                    var $level1Mean = $listMeansAndEx.eq(i);
+
+                    level1Obj.phase = $level1Phase.find('.trg .ind').text();
+                    level1Obj.mean = $level1Mean.find('.trg').first().find('.ind').text();
+
+                    level1Obj.exampls = [];
+                    $level1Mean.find('.trg').eq(1).find('> .exg .ex').each(function() {
+                        var ex = '';
+                        if($(this).find('.form-groups').length > 0) {
+                            ex += '<span class="level1-strong-mean">' + $(this).find('.form-groups strong').text() + '</span>';
+                        }
+                        if($(this).find('.grammatical_note').length > 0) {
+                            ex += '<span class="no-object">' + $(this).find('.grammatical_note').text() + '</span>';
+                        }                            
+                        ex += $(this).find('em').text();
+                        level1Obj.exampls.push(ex);
+                    });
+                    if(level1Obj.exampls.length < 2) {
+                        var $otherExamples = $level1Mean.find('.trg').eq(1).find('> .examples .ex');
+                        if($otherExamples.length > 0) {
+                            $otherExamples.slice(0, 2 - level1Obj.exampls.length).each(function() {                                
+                                var ex = '';
+                                if($(this).find('.form-groups').length > 0) {
+                                    ex += '<span class="level1-strong-mean">' + $(this).find('.form-groups strong').text() + '</span>';
+                                }
+                                if($(this).find('.grammatical_note').length > 0) {
+                                    ex += '<span class="no-object">' + $(this).find('.grammatical_note').text() + '</span>';
+                                }                            
+                                ex += $(this).find('em').text();
+                                level1Obj.exampls.push(ex);
+                            });
+                        } 
+                    }                    
+
+                    etymologyObj.level1.push(level1Obj);
+                }                
+                translateToEn.etymologys.push(etymologyObj);
+            });            
+        }
+
+        console.log(JSON.stringify(translateToEn));
               
 
         //var con = this.wait();       
